bd712b24757b370973581b2f157e1e15e540066c
ggappsww@gmail.com <ggappsww@gmail.com>
2024-08-05 19:16:13 -0500

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,9 @@
 import logging
 import sys
 
+from pathlib import Path
+sys.path.append(str(Path('src').parent.parent))  
+
 def error_message_detail(error, error_detail: sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
